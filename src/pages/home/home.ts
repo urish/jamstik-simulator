2d5ae5f5166808925da9484dd107acbdf5c53227
Uri Shaked <uri@salsa4fun.co.il>
2017-11-01 21:08:54 +0200

    feat: basic functionality working
    simulate strings, accept bluetooth connections and send midi messages

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { DeviceProvider } from './../../providers/device/device';
+
+const stringTuning = [64, 59, 55, 50, 45, 40];
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  frets = [0, 1, 2, 3, 4, 5];
+  strings = [0, 1, 2, 3, 4, 5];
 
-  constructor(public navCtrl: NavController) {
+  private activeNotes = new Set<string>();
 
+  constructor(public navCtrl: NavController, private device: DeviceProvider) {
+    document.addEventListener('deviceready', () => {
+      device.start();
+    }, false);
   }
 
+  play(fret: number, stringId: number) {
+    const id = `${fret}/${stringId}`;
+    const pitch = stringTuning[stringId] + fret;
+    this.activeNotes.add(id);
+    this.device.noteOn(stringId, pitch);
+    setTimeout(() => {
+      this.device.noteOff(stringId, pitch);
+      this.activeNotes.delete(id);
+    }, 300);
+  }
+
+  isActive(fret: number, stringId: number) {
+    const id = `${fret}/${stringId}`;
+    return this.activeNotes.has(id);
+  }
 }
---
+++ src/providers/device/device.ts
@@ -0,0 +1,99 @@
+import { Injectable } from '@angular/core';
+
+declare var bluetoothle;
+
+const MIDI_SERVICE = '03b80e5a-ede8-4b33-a751-6ce34ec4c700';
+const MIDI_CHARACTERISTIC = '7772e5db-3868-4112-a1a9-f2669d106bf3';
+
+@Injectable()
+export class DeviceProvider {
+  remoteAddress: string | null;
+
+  start() {
+    if (typeof bluetoothle !== 'undefined') {
+      bluetoothle.initialize(result => this.onBleReady(result), {
+        request: true
+      });
+    } else {
+      console.error('Please install cordova-plugin-bluetoothle');
+    }
+  }
+
+  sendMidiMessage(timestamp: number, status: number, data1: number, data2: number) {
+    if (!this.remoteAddress) {
+      console.error('Unable to send midi message: not connected');
+      return;
+    }
+
+    let header = 0x80 | ((timestamp >> 7) & 0x3f);
+    let byte2 = 0x80 | (timestamp & 0x7f);
+
+    bluetoothle.notify(
+      result => console.log('notify result', result),
+      error => console.error('notify failed', error), {
+        address: this.remoteAddress,
+        service: MIDI_SERVICE,
+        characteristic: MIDI_CHARACTERISTIC,
+        value: bluetoothle.bytesToEncodedString([header, byte2, status, data1, data2])
+      });
+  }
+
+  noteOn(channel: number, note: number, velocity: number = 127) {
+    this.sendMidiMessage(0, 0x90 | (channel & 0xf), note, velocity);
+  }
+
+  noteOff(channel: number, note: number) {
+    this.sendMidiMessage(0, 0x80 | (channel & 0xf), note, 0);
+  }
+
+  private onBleReady(result) {
+    console.log('onBleReady', result);
+    bluetoothle.initializePeripheral(status => this.bleCallback(status), err => this.onError(err), {
+      request: true
+    });
+  }
+
+  private bleCallback(event) {
+    console.log('BLE callback', event);
+    if (event.status === 'enabled') {
+      bluetoothle.addService(result => console.log('ok', result), err => console.log('failed!', err), {
+        service: MIDI_SERVICE,
+        characteristics: [
+          {
+            uuid: MIDI_CHARACTERISTIC,
+            permissions: {
+              read: true
+            },
+            properties: {
+              read: true,
+              notify: true
+            }
+          }
+        ]
+      });
+
+      bluetoothle.startAdvertising(result => console.log('advertising', result), err => console.error('advertising failed', err), {
+        services: [MIDI_SERVICE],
+        service: MIDI_SERVICE,
+        name: 'JAMSTIK-SIM',
+        mode: 'lowLatency',
+        connectable: true,
+        timeout: 180000,
+        powerLevel: 'high'
+      });
+    }
+    if (event.status === 'writeRequested') {
+      console.log('write request ignored');
+    }
+    if (event.status === 'subscribed') {
+      this.remoteAddress = event.address;
+    }
+    if (event.StatusBar === 'disconnected') {
+      this.remoteAddress = null;
+    }
+  }
+
+  onError(err) {
+    console.log('BLE error', err);
+  }
+}
